fa82b90aa13af37469567b97810f80b9fbc8b7e7
Gyanendra <gyanen56@gmail.com>
2024-05-08 13:03:11 +1000

    display all users before pagination

--- data.js
+++ data.js
@@ -3,9 +3,8 @@ const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
 
 const data = async ()=>{
    const response =  await fetch(url)
-   console.log(response)
    const result = await response.json()
-   console.log(result)
+
    return result;
 }
 
--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 import {data} from './data.js'
 
 
-const Employees = await data()
+const employees = await data()
 
+const row = document.querySelector('.js-row')
+let html=''
+employees.forEach((employee)=>{
+    const {id, avatar_url, login,url } = employee
+   html += `
+            <div class=" col-11 col-md-2 p-3  text-capitalize text-center bg-light  m-2 rounded shadow-lg ">
+                <img src="${avatar_url}" alt="hello" class="img rounded-circle">
+                <p class="mt-2">${login}</p>
+                <a href="${url}" class="bg-primary text-light p-2 rounded-pill text-decoration-none ps-3 pe-3"> View Profile</a>
+            </div>
+            `
+}
+)
+row.innerHTML+=html
